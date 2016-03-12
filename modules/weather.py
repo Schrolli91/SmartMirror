c967d5efc2d478fab2a26b98d60b16bf67a62554
Bastian Schroll <bastian@dieschrollis.de>
2016-03-12 16:00:40 +0100

    changes in weather.py
    - codeumfang reduziert
    - kleine änderungen

--- modules/weather.py
+++ modules/weather.py
@@ -34,46 +34,39 @@ class weather:
         #Aktuelle Temperatur darstellen
         self.tempakt = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
         self.tempakt.place(x=xPos, y=yPos+2*self.stepy, anchor=N)
-        #Max. Temerpatur darstellen
-        self.tempmax = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
-        self.tempmax.place(x=xPos, y=yPos+3*self.stepy, anchor=N)
-        #Min. Temperatur darstellen
-        self.tempmin = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
-        self.tempmin.place(x=xPos, y=yPos+4*self.stepy, anchor=N)
+        #Max. Min. Temerpatur darstellen
+        self.tempMaxMin = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
+        self.tempMaxMin.place(x=xPos, y=yPos+3*self.stepy, anchor=N)
         #Wettersituation darstellen
         self.wettersituation = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
-        self.wettersituation.place(x=xPos, y=yPos+5*self.stepy, anchor=N)
+        self.wettersituation.place(x=xPos, y=yPos+4*self.stepy, anchor=N)
         self.wettericon = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
         self.wettericon.place(x=xPos+250, y=yPos, anchor=N)
         #Wetter am NÃ¤chsten Tag
         self.wettermorgen = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","main_font"), bg='black')
-        self.wettermorgen.place(x=xPos, y=yPos+7*self.stepy, anchor=N)
-        #Max. Temperatur am nÃ¤chsten Tag
-        self.tempmaxtag2 = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
-        self.tempmaxtag2.place(x=xPos, y=yPos+8*self.stepy, anchor=N)
-        #Min. Temperatur am nÃ¤chsten Tag
-        self.tempmintag2 = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
-        self.tempmintag2.place(x=xPos, y=yPos+9*self.stepy, anchor=N)
+        self.wettermorgen.place(x=xPos, y=yPos+6*self.stepy, anchor=N)
+        #Max. Min. Temperatur am nÃ¤chsten Tag
+        self.tempMaxMin2 = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
+        self.tempMaxMin2.place(x=xPos, y=yPos+7*self.stepy, anchor=N)
         #Wettersituation am nÃ¤chsten Tag
         self.wettersit2 = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black')
-        self.wettersit2.place(x=xPos, y=yPos+10*self.stepy, anchor=N)
+        self.wettersit2.place(x=xPos, y=yPos+8*self.stepy, anchor=N)
         #Upadte routine
         self.update()
 
     def update(self):
 
         self.fetch_data()
-        self.ort.configure(text=self.ort1)
+        self.ort.configure(text="Wetterbericht fuer " + self.Stadt + " in " + self.Land)
         self.wetterheute.configure(text=" Heutiges Wetter")
-        self.tempakt.configure(text=self.tempakt1)
-        self.tempmax.configure(text=self.tempmax1)
-        self.tempmin.configure(text=self.tempmin1)
+        self.tempakt.configure(text="Temperatur: " +str(self.Temperatur) + " C")
+        self.tempMaxMin.configure(text="(" +str(self.Wetter[0][2])+ " C bis " +str(self.Wetter[0][3])+ " C)")
         self.wettersituation.configure(text=self.wettersituation1)
         self.wettericon.configure(image=self.wettericonauswahl)
+
         self.wettermorgen.configure(text=" Wetter am naechsten Tag")
-        self.tempmaxtag2.configure(text=self.tempmax2)
-        self.tempmintag2.configure(text=self.tempmin2)
-        self.wettersit2.configure(text=self.wettersituation2)
+        self.tempMaxMin2.configure(text="Temperatur " +str(self.Wetter[1][2])+ " C bis " +str(self.Wetter[1][3])+ " C")
+        self.wettersit2.configure(text="Wettersituation: " + self.Wetter[1][4])
         self.tempakt.after(self.config.get("weather","update_interval"), self.update)
 
 
@@ -82,61 +75,62 @@ class weather:
 
         self.Wetter = [["", "", "", "", ""] , ["", "", "", "", ""]]
 
-    # URL oeffnen und XML Daten einlesen
-        self.Baum = urllib.request.urlopen('http://weather.yahooapis.com/forecastrss?w=12837685').read()
-    # XML Daten parsen und in Baumstruktur anordnen
-        self.Baum = parseString(self.Baum)
+        # URL oeffnen und XML Daten einlesen
+        self.tree = urllib.request.urlopen('http://weather.yahooapis.com/forecastrss?w=12837685').read()
+        # XML Daten parsen und in treestruktur anordnen
+        self.tree = parseString(self.tree)
 
-    # Ort einlesen
-        self.Ort = self.Baum.getElementsByTagName('yweather:location')[0]
+        # Ort einlesen
+        self.Ort = self.tree.getElementsByTagName('yweather:location')[0]
         self.Stadt = self.Ort.attributes["city"].value
         self.Land = self.Ort.attributes["country"].value
 
-    # Datum einlesen
-        self.Datum = self.Baum.getElementsByTagName('lastBuildDate')[0].firstChild.data
+        # Datum einlesen
+        self.build_date = self.tree.getElementsByTagName('lastBuildDate')[0].firstChild.data
 
-    # Koordinaten auslesen
-        self.Geo_Lat = self.Baum.getElementsByTagName('geo:lat')[0].firstChild.data
-        self.Geo_Long = self.Baum.getElementsByTagName('geo:long')[0].firstChild.data
+        # Koordinaten auslesen
+        self.Geo_Lat = self.tree.getElementsByTagName('geo:lat')[0].firstChild.data
+        self.Geo_Long = self.tree.getElementsByTagName('geo:long')[0].firstChild.data
 
-    # Wetterdaten von heute einlesen
-        self.Today = self.Baum.getElementsByTagName('yweather:condition')[0]
+        # Wetterdaten von heute einlesen
+        self.Today = self.tree.getElementsByTagName('yweather:condition')[0]
 
-    # Wind einlesen
-        self.Wind = self.Baum.getElementsByTagName('yweather:wind')[0]
+        # Wind einlesen
+        self.Wind = self.tree.getElementsByTagName('yweather:wind')[0]
         self.Richtung = float(self.Wind.attributes["direction"].value)
         self.Speed = float(self.Wind.attributes["speed"].value)
         self.Speed = round((self.Speed*1.609),2)
 
-    # Sonnenaufgang einlesen
-        self.Sonne = self.Baum.getElementsByTagName('yweather:astronomy')[0]
+        # Sonnenaufgang einlesen
+        self.Sonne = self.tree.getElementsByTagName('yweather:astronomy')[0]
         self.Aufgang = self.Sonne.attributes["sunrise"].value
         self.Untergang = self.Sonne.attributes["sunset"].value
 
-    # Wettertext einlesen
+        # Wettertext einlesen
         self.Wetterlage = self.Today.attributes["text"].value
 
-    # Temperatur in Fahrenheit einlesen, umrechnen und runden
+        # Temperatur in Fahrenheit einlesen, umrechnen und runden
         self.Temperatur = float(self.Today.attributes["temp"].value)
         self.Temperatur = round((self.Temperatur - 32.0) * (5.0 / 9.0) , 1)
 
-    # Daten in einer Liste speichern
+        # Daten in einer Liste speichern
         for Counter in range(2):
 
-    # Wetterdaten fuer die beiden Tage einlesen
-    # Daten einlesen
-            self.Future = self.Baum.getElementsByTagName('yweather:forecast')[Counter]
+            # Wetterdaten fuer die beiden Tage einlesen
+            # Daten einlesen
+            self.Future = self.tree.getElementsByTagName('yweather:forecast')[Counter]
 
-    # Daten verarbeiten
+            # Daten verarbeiten
             self.Wetter[Counter][0] = self.Future.attributes["day"].value
             self.Wetter[Counter][1] = self.Future.attributes["date"].value
             self.Wetter[Counter][2] = float(self.Future.attributes["low"].value)
             self.Wetter[Counter][3] = float(self.Future.attributes["high"].value)
             self.Wetter[Counter][4] = self.Future.attributes["text"].value
-    # Umrechnen der Temperatur in Grad Celsius
+            # Umrechnen der Temperatur in Grad Celsius
             self.Wetter[Counter][2] = round((self.Wetter[Counter][2] - 32.0) * (5.0 / 9.0) , 1)
             self.Wetter[Counter][3] = round((self.Wetter[Counter][3] - 32.0) * (5.0 / 9.0) , 1)
-    # Umrechung Windrichtung
+
+        # Umrechung Windrichtung
         if self.Richtung >= 0:
             self.Windrichtung = "Nord"
         elif self.Richtung >= 45:
@@ -156,16 +150,8 @@ class weather:
         else:
             self.Windrichtung = "Fehler!"
 
-        self.ort1 = " Wetterbericht für " + self.Stadt + " in " + self.Land
-        self.tempakt1 = " Temperatur: " +str(self.Temperatur) + " C Celsius "
-        self.tempmax1 = " Max. Temperatur: " + str(self.Wetter[0][3]) + " C Celsius "
-        self.tempmin1 = " Min. Temperatur: " + str(self.Wetter[0][2]) + " C Celsius "
-        self.tempmax2 = " Max. Temperatur: " + str(self.Wetter[1][3]) + " C Celsius "
-        self.tempmin2 = " Min. Temperatur: " + str(self.Wetter[1][2]) + " C Celsius "
-        self.wettersituation2 = " Wettersituation: " + self.Wetter[1][4]
 
-    #Funktion für Wettersituation
-
+        #Funktion für Wettersituation
         self.wettersituation1 = " Wettersituation: " + self.Wetterlage
         if self.Wetterlage == "Mostly Cloudy":
             self.wettericonauswahl = self.cloud
