df76447f89c758038186c632bac5b0e68bdf9959
Schrolli91 <bastian@dieschrollis.de>
2016-03-12 13:04:03 +0100

    initial commit

---
+++ mirror.py
@@ -0,0 +1,51 @@
+#########
+#
+# MirrorOS
+# simple OS for smart Mirrors
+#
+# Version 0.1
+# 2016 by Bastian Schroll and Matthias Kittler
+#
+############
+
+from tkinter import *
+import configparser
+
+#import the modules
+from modules.clock import clock
+from modules.date import date
+from modules.weather import weather
+
+
+#read the config file object
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+#generate the tkinter window object
+root = Tk()
+root.title("MirrorOS")
+
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()#w and h for the display resolutions
+root.overrideredirect(1)#hide the window border
+root.geometry("%dx%d+0+0" % (w, h))#set the window size
+
+root.configure(background='black')#set background color
+root.bind("<Escape>", lambda e: e.widget.quit())#Escape Button to quit the window
+
+#show the MirrorOS Logo in the middle of the screen
+logo = Label(root, text="MirrorOS", fg="white", bg="black", font="Verdana 40 bold")
+logo.place(x=w/2, y=50, anchor=CENTER)
+footer = Label(root, text="quit with [ESC]", fg="gray", bg="black", font="Verdana 10 bold")
+footer.place(x=w/2, y=h, anchor=S)
+
+
+if config.getboolean("Modules","clock"):
+    uhr = clock(root, config) #build new clock
+
+if config.getboolean("Modules","date"):
+    datum = date(root, config) #build new date
+
+if config.getboolean("Modules","weather"):
+    wetter = weather(root, config) #build new date
+
+root.mainloop()
---
+++ modules/date.py
@@ -0,0 +1,21 @@
+"""
+Date module for MirrorOS
+Autor: Bastian Schroll
+"""
+
+from tkinter import *
+import time
+
+class date:
+    def __init__(self, window, config):
+        self.window = window
+        self.config = config
+
+        self.date = Label(self.window, fg=self.config.get("date","color"), font=self.config.get("date","font"), bg='black')
+        self.date.place(x=self.window.winfo_screenwidth()/2, y=self. window.winfo_screenheight()/2-80, anchor=N)
+
+        self.update() #starts his own update routine
+
+    def update(self):
+        self.date.configure(text=time.strftime(self.config.get("date","format")))
+        self.date.after(self.config.getint("date","update_interval"), self.update)
