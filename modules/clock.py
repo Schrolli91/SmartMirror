57bf77a3a669808b5f7f1e38b18a0ad051c58641
Bastian Schroll <bastian@dieschrollis.de>
2016-03-13 09:20:37 +0100

    insert logging into time an date module

--- modules/clock.py
+++ modules/clock.py
@@ -6,6 +6,7 @@ Autor: Bastian Schroll
 
 from tkinter import *
 import time
+import logging
 
 class clock:
     def __init__(self, window, config, xPos, yPos, anc="n"):
@@ -15,8 +16,10 @@ class clock:
         self.clock = Label(self.window, fg=self.config.get("clock","color"), font=self.config.get("clock","font"), bg='black')
         self.clock.place(x=xPos, y=yPos, anchor=anc)
 
+        logging.debug("generated")
         self.update() #starts his own update routine
 
     def update(self):
+        logging.debug("update widget")
         self.clock.configure(text=time.strftime(self.config.get("clock","format")))
         self.clock.after(self.config.getint("clock","update_interval"), self.update)
--- modules/date.py
+++ modules/date.py
@@ -6,6 +6,7 @@ Autor: Bastian Schroll
 
 from tkinter import *
 import time
+import logging
 
 class date:
     def __init__(self, window, config, xPos, yPos, anc="n"):
@@ -15,8 +16,10 @@ class date:
         self.date = Label(self.window, fg=self.config.get("date","color"), font=self.config.get("date","font"), bg='black')
         self.date.place(x=xPos, y=yPos, anchor=anc)
 
+        logging.debug("generated")
         self.update() #starts his own update routine
 
     def update(self):
+        logging.debug("update widget")
         self.date.configure(text=time.strftime(self.config.get("date","format")))
         self.date.after(self.config.getint("date","update_interval"), self.update)
