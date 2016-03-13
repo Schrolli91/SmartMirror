74f9ff49e99d4d1da441cad02270637f42cf435c
Bastian Schroll <bastian@dieschrollis.de>
2016-03-13 09:13:45 +0100

    added logging and error image to weather_json

--- modules/weather_json.py
+++ modules/weather_json.py
@@ -7,13 +7,15 @@ and downloads the condition image
 Autor: Bastian Schroll
 """
 
-from xml.dom.minidom import *
 from tkinter import *
+import logging
+
+#for json download
 import urllib.request
 import json
 
 #for the image download
-from urllib.request import urlopen
+#from urllib.request import urlopen
 import io
 import base64
 
@@ -44,19 +46,23 @@ class jsonweather:
         self.weather_forecast = Label(self.window, fg=self.config.get("weather","color"), font=self.config.get("weather","font"), bg='black', justify=LEFT)
         self.weather_forecast.place(x=xPos, y=yPos+8*self.yStep, anchor=anc)
 
-
+        logging.debug("generated")
         self.update()
 
     def update(self):
+        logging.debug("load new weather data")
         self.loading.configure(text="Aktualisiere Wetterdaten ...")
         self.window.update() #redraw window
         self.fetch_data()
         self.loading.configure(text="")
 
+        logging.debug("refresh the widgets")
+
         self.headline.configure(text="Wetterbericht für " + str(self.wetter["name"]) +" ("+ str(self.wetter["sys"]["country"]) +")")
         self.main_today.configure(text="Heutiges Wetter:")
 
-        self.weather_today.configure(text=str(self.wetter["weather"][0]["description"]) +"\n"+
+        self.weather_today.configure(text=
+            str(self.wetter["weather"][0]["description"]) +"\n"+
             "Temperatur: "+ str(round(self.wetter["main"]["temp"],1)) +" °C"+"\n"+
             "Min: "+ str(round(self.wetter["main"]["temp_min"],1)) +" °C / Max: "+ str(round(self.wetter["main"]["temp_max"],1)) +" °C"+"\n"+
             "Luftdruck: "+ str(round(self.wetter["main"]["pressure"])) +" hPa"+"\n"+
@@ -68,27 +74,33 @@ class jsonweather:
 
         self.main_forecast.configure(text="Vorhersage:")
 
-        self.weather_forecast.configure(text="Morgen: "+ str(self.forecast["list"][0]["weather"][0]["description"]) +" ("+ str(round(self.forecast["list"][0]["temp"]["day"],1)) +"°C)\n"+
+        self.weather_forecast.configure(text=
+            "Morgen: "+ str(self.forecast["list"][0]["weather"][0]["description"]) +" ("+ str(round(self.forecast["list"][0]["temp"]["day"],1)) +"°C)\n"+
             "Übermorgen: "+ str(self.forecast["list"][1]["weather"][0]["description"]) +" ("+ str(round(self.forecast["list"][1]["temp"]["day"],1)) +"°C)\n"
             )
 
         self.window.after(self.config.get("weather","update_interval"), self.update)
 
     def fetch_data(self):
-
+        logging.debug("load actual weather json from OWM")
         #get json weather data from today
         req = urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q="+ self.config.get("weather","city") +"&lang="+ self.config.get("weather","lang") +"&units=metric&appid="+ self.config.get("weather","api_key"))
         encoding = req.headers.get_content_charset()
         self.wetter = json.loads(req.read().decode(encoding))
 
-
+        logging.debug("load weather forecast json from OWM")
         #get json weather forecast
         req = urllib.request.urlopen("http://api.openweathermap.org/data/2.5/forecast/daily?q="+ self.config.get("weather","city") +"&lang="+ self.config.get("weather","lang") +"&units=metric&cnt=2&appid="+ self.config.get("weather","api_key"))
         encoding = req.headers.get_content_charset()
         self.forecast = json.loads(req.read().decode(encoding))
 
-        #image download
-        image_url = "http://openweathermap.org/img/w/"+ str(self.wetter["weather"][0]["icon"]) +".png"
-        image_byt = urlopen(image_url).read()
-        image_b64 = base64.encodestring(image_byt)
-        self.photo = PhotoImage(data=image_b64)
+        try:
+            logging.debug("load actual weather image from OWM")
+            #image download
+            image_url = "http://openweathermap.org/img/w/"+ str(self.wetter["weather"][0]["icon"]) +".png"
+            image_byt = urllib.request.urlopen(image_url).read()
+            image_b64 = base64.encodestring(image_byt)
+            self.photo = PhotoImage(data=image_b64)
+        except:
+            logging.exception("error while loading, set error.png")
+            self.photo = PhotoImage(file="modules/error.png")
