d6fdbd9a81c5a45dce3bd11612545b069efe0d76
Matthias Kittler <matze_kittler@yahoo.de>
2016-03-17 07:52:04 +0100

    tausch calendar_mirror zu threating calendar_mirror

--- modules/calendar_mirror.py
+++ modules/calendar_mirror.py
@@ -1,122 +1,105 @@
 #-*- encoding: utf-8 -*-
 """
-Calendar module for MirrorOS
+Calendar Modul with threading
 Autor: Matthias Kittler
 """
 
-from __future__ import print_function
-import httplib2
-import os
-
-from apiclient import discovery
-import oauth2client
-from oauth2client import client
-from oauth2client import tools
 from tkinter import *
-import datetime
+import threading
 import logging
+import time
 
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/calendar-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'MirrorOS'
-
-class calendar_mirror:
-    def __init__(self, window, config, xPos, yPos, anc="n"):
+class calendar_mirror(threading.Thread):
+    def __init__(self, window, config, xPos, yPos, anc="nw"):
+        threading.Thread.__init__(self)
+        self.name = __name__
+        self.daemon = True
         try:
-            logging.debug("generate " + __name__)
+            logging.debug("load " + __name__)
+
             self.window = window
             self.config = config
-
+            self.xPos = xPos
+            self.yPos = yPos
+            self.anc = anc
             self.yStep = 22
 
-            #label for the refresh info
+            ##############
+            # init section
+            
             self.loading = Label(self.window, fg=self.config.get("calendar_mirror","main_color"), font=self.config.get("calendar_miror","font"), bg='black')
-            self.loading.place(x=xPos, y=yPos+10*self.yStep, anchor=anc)
+            self.loading.place(x=self.xPos, y=self.yPos+2*self.yStep, anchor=self.anc)
 
             self.headline = Label(self.window, fg=self.config.get("calendar_mirror","color"), font=self.config.get("calendar_mirror","main_font"), bg='black')
-            self.headline.place(x=xPos, y=yPos, anchor=anc)
+            self.headline.place(x=self.xPos, y=self.yPos, anchor=self.anc)
 
-            self.update() #starts his own update routine
-
-        except:
-            logging.exception("cannot generate " + __name__)
-
-
-    def update(self):
-        try:
-            logging.debug("update " + __name__)
-
-            self.loading.configure(text="Aktualisiere Kalenderdaten ...")
-            self.window.update() #redraw window
-            self.fetch_data() #reload data from web
-            self.loading.configure(text="")
-
-            logging.debug("refresh the widgets")
-            #refresh the widgets for events
-            self.headline.configure(text=" Einträge im Kalender: "
-
-            self.window.after(self.config.getint("calendar_mirror","update_interval"), self.update)
+            self.calendar_text = Label(self.window, fg=self.config.get("calendar_mirror","color"), font=self.config.get("news", "font"), bg='black')
+            self.calendar_text.place(x=self.xPos, y=self.yPos+4*self.yStep, anchor=self.anc)
+            # init section
+            ##############
 
         except:
-            logging.exception("cannot refresh the data")
+            logging.exception("cannot load " + __name__)
 
 
+    def run(self):
+        logging.debug("run " + __name__)
+        while 1: #infinite loop from thread - on exit, thread dies
+            try:
+                logging.debug("update " + __name__)
 
-    def fetch_data(self):
-        try:
-            logging.debug("load entries")
-            #get events
+                ##############
+                # code section
 
-            """Gets valid user credentials from storage.
+                """Gets valid user credentials from storage.
 
-        If nothing has been stored, or if the stored credentials are invalid,
-        the OAuth2 flow is completed to obtain the new credentials.
+            If nothing has been stored, or if the stored credentials are invalid,
+            the OAuth2 flow is completed to obtain the new credentials.
 
-        Returns:
-            Credentials, the obtained credential.
-        """
-            home_dir = os.path.expanduser('C:')
-            credential_dir = os.path.join(home_dir, '.credentials')
-            if not os.path.exists(credential_dir):
-                os.makedirs(credential_dir)
-            credential_path = os.path.join(credential_dir,
-                                           'calendar-python.json')
+            Returns:
+                Credentials, the obtained credential.
+            """
+                home_dir = os.path.expanduser('C:')
+                credential_dir = os.path.join(home_dir, '.credentials')
+                if not os.path.exists(credential_dir):
+                    os.makedirs(credential_dir)
+                credential_path = os.path.join(credential_dir,
+                                               'calendar-python.json')
 
-            store = oauth2client.file.Storage(credential_path)
-            credentials = store.get()
-            if not credentials or credentials.invalid:
-                flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-                flow.user_agent = APPLICATION_NAME
-                if flags:
-                    credentials = tools.run_flow(flow, store, flags)
-                else: # Needed only for compatibility with Python 2.6
-                    credentials = tools.run(flow, store)
-                print('Storing credentials to ' + credential_path)
+                store = oauth2client.file.Storage(credential_path)
+                credentials = store.get()
+                if not credentials or credentials.invalid:
+                    flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+                    flow.user_agent = APPLICATION_NAME
+                    if flags:
+                        credentials = tools.run_flow(flow, store, flags)
+                    else: # Needed only for compatibility with Python 2.6
+                        credentials = tools.run(flow, store)
+                    print('Storing credentials to ' + credential_path)
 
-            http = credentials.authorize(httplib2.Http())
-            service = discovery.build('calendar', 'v3', http=http)
+                http = credentials.authorize(httplib2.Http())
+                service = discovery.build('calendar', 'v3', http=http)
 
-            now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-            print('Getting the upcoming 10 events')
-            eventsResult = service.events().list(
-                calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-                orderBy='startTime').execute()
-            events = eventsResult.get('items', [])
+                now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+                print('Getting the upcoming 10 events')
+                eventsResult = service.events().list(
+                    calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+                    orderBy='startTime').execute()
+                events = eventsResult.get('items', [])
 
-            if not events:
-                print('No upcoming events found.')
-            for event in events:
-                start = event['start'].get('dateTime', event['start'].get('date'))
-                print(start, event['summary'])
+                if not events:
+                    print('No upcoming events found.')
+                for event in events:
+                    start = event['start'].get('dateTime', event['start'].get('date'))
+                    print(start, event['summary'])
 
-        except:
-            logging.exception("cannot fetch data from web")
+                self.loading.configure(text="Aktualisiere Kalenderdaten ...")
+                self.loading.configure(text="")
+                self.headline.configure(text=" Einträge im Kalender: ")
+                # code section
+                ##############
+
+            except:
+                logging.exception("cannot update " + __name__)
+            finally:
+                time.sleep(self.config.getfloat("calendar_mirror","update_intervall"))
