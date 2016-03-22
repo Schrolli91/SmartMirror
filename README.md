e193769a1e5c83b31fdc6c3736ab3799add8690e
Bastian Schroll <bastian@dieschrollis.de>
2016-03-22 11:01:51 +0100

    edit readme

--- README.md
+++ README.md
@@ -11,20 +11,29 @@ Jedes Modul wird in einem eigenen Thread ausgeführt. Durch die dadurch erreicht
 ## Aktuelle Module
 Follgende Module sind derzeit in MirrorOS implementiert oder geplant:
 
+:white_check_mark: fertig implementiert | :x: bisher nicht implementiert | :question: evtl. geplant
+
 |Modul|Beschreibung|Fertig|
 |-----|------------|:----:|
 |Uhrzeit|Zeigt die aktuelle Uhrzeit an|:white_check_mark:|
 |Datum|Zeigt das aktuelle Datum an|:white_check_mark:|
 |Wetter|Zeigt das Aktuelle Wetter + 2 Tages  Vorhersage an|:white_check_mark:|
-|Kalender|Zeigt die nächsten Termine aus dem Google Calendar|:x:|
-|Focus News|Liest den RSS Feed der Focus Eilmeldungen|:white_check_mark:|
+|Kalender|Zeigt die nächsten Termine aus dem Google Calendar|:white_check_mark:|
+|Focus News|Liest den RSS Feed der Focus Eilmeldungen aus|:white_check_mark:|
 |Begrüßung|Uhrzeitabhängige Begrüßungen|:white_check_mark:|
+|Spracherkennung|Steuerung des Mirrors durch Spracheingaben|:x:|
+|Text to Speech|Sprachausgabe durch den Mirror|:x:|
+|Wikipedia|Suche auf Wikipedia|:x:|
+|Gesichtserkennung|Benutzer durch Webcam unterscheiden|:question:|
+|Maps|Zeigt eine Karte oder Route an|:question:|
+|Verkehr|Zeigt die aktuelle Verkehrslage auf einer Route an|:question:|
+
 
 #### Debug und Entwickler Module
 |Modul|Beschreibung|Fertig|
 |-----|------------|:----:|
 |Log View|liest die letzten 10 Meldungen aus dem Logfile aus|:white_check_mark:|
-|Thread Status|Listet alle aktuel laufenen Threads auf|:white_check_mark:|
+|Modul Status|Listet alle aktuel laufenen Module auf|:white_check_mark:|
 
 
 ## Installation
---
+++ ideas/wiki.py
@@ -0,0 +1,16 @@
+https://de.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=1&explaintext=1&utf8=1&titles=Albert+Einstein
+
+{
+  "batchcomplete": "",
+  "query": {"pages": {"1278360": {
+        "pageid": 1278360,
+        "ns": 0,
+        "title": "Albert Einstein",
+        "extract": "Albert Einstein (* 14. März 1879 in Ulm; † 18. April 1955 in Princeton, New Jersey) war ein theoretischer Physiker. Seine Forschungen zur Struktur von Materie, Raum und Zeit sowie dem Wesen der Gravitation veränderten maßgeblich das physikalische Weltbild. Er gilt daher als einer der bedeutendsten Physiker aller Zeiten.
+Einsteins Hauptwerk, die Relativitätstheorie, machte ihn weltberühmt. Im Jahr 1905 erschien seine Arbeit mit dem Titel Zur Elektrodynamik bewegter Körper, deren Inhalt heute als spezielle Relativitätstheorie bezeichnet wird. 1915 publizierte Einstein die allgemeine Relativitätstheorie. Auch zur Quantenphysik leistete er wesentliche Beiträge. „Für seine Verdienste um die theoretische Physik, besonders für seine Entdeckung des Gesetzes des photoelektrischen Effekts“, erhielt er den Nobelpreis des Jahres 1921, welcher ihm 1922 überreicht wurde. Seine theoretischen Arbeiten spielten – im Gegensatz zur verbreiteten Meinung – beim Bau der Atombombe und der Entwicklung der Kernenergie nur eine indirekte Rolle.
+Albert Einstein gilt als Inbegriff des Forschers und Genies. Er nutzte seine außerordentliche Bekanntheit auch außerhalb der naturwissenschaftlichen Fachwelt bei seinem Einsatz für Völkerverständigung und Frieden. In diesem Zusammenhang verstand er sich selbst als Pazifist, Sozialist und Zionist.
+Im Laufe seines Lebens war Einstein Staatsbürger mehrerer Länder: Durch Geburt besaß er die württembergische Staatsbürgerschaft. Von 1896 bis 1901 staatenlos, danach Staatsbürger der Schweiz, war er 1911/12 in Österreich-Ungarn auch Bürger Österreichs. Von 1914 bis 1932 lebte Einstein in Berlin und war als Bürger Preußens erneut Staatsangehöriger im Deutschen Reich. Mit der „Machtergreifung“ Hitlers gab er 1933 den deutschen Pass endgültig ab. Zu seinem seit 1901 geltenden Schweizer Bürgerrecht kam ab 1940 noch die amerikanische Staatsbürgerschaft.
+↑ Physics World Magazine, 1999 – Vergleiche: BBC-Artikel.
+↑ Markus Pössel: Von E=mc² zur Atombombe. Auf: einstein-online.info vom Max-Planck-Institut für Gravitationsphysik."
+      }}}
+}
