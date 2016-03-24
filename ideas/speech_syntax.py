891b225c4d996222f7848e9ed73bb3eac4b1cac8
Bastian Schroll <bastian@dieschrollis.de>
2016-03-24 13:34:18 +0100

    improve regex for speech parsing in ideas

--- ideas/speech_syntax.py
+++ ideas/speech_syntax.py
@@ -1,11 +1,65 @@
 import re
 
-pattern = "(er|sie) soll (dich|mich|uns) mal anrufen (heute)"
+
+# line = "starte wiki schuhmacher"
+# pattern = "((?:starte|beginne){0,1}\s?(?:wikipedia|wiki){1}\s?(?:suche){0,1}\s?(?P<test>.*))"
+#
+# matchObj = re.match(pattern, line, re.MULTILINE|re.IGNORECASE)
+#
+# if matchObj:
+#    print ("matchObj.group() : ", matchObj.group())
+#    print ("matchObj.group(1) : ", matchObj.group(1))
+#    print ("matchObj.group(2) : ", matchObj.group("test"))
+# else:
+#    print ("No match!!")
+#
+#
+#
+# exit()
+
+#regex suche
+#https://regex101.com/r/gV0dP7/8
+
+
+checkFor = []
+
+checkFor.append("starte wiki schuhmacher")
+checkFor.append("starte wikipedia das ist ein kleiner test")
+checkFor.append("starte eine google suche")
+checkFor.append("beginne wikipedia suche das ist ein test")
+checkFor.append("wikipedia albert einstein")
+checkFor.append("hallo test")
+
+pattern = "(starte|beginne)? (durchsuche)? (wikipedia|wiki)+ (suche)? (nach)? *suchStr"
+#pattern = "# in (wiki|wikipedia)+ suchen"
+
 pattern = pattern.replace("(", "(?:") #um keinen eigenen match für die gruppe zu erstellen
-pattern = pattern.replace(")", ")?") #gruppe genau kein oder einmal
+pattern = pattern.replace(")?", "){0,1}") #gruppe kann, muss aber nicht
+pattern = pattern.replace(")+", "){1}") #gruppe muss
+#pattern = pattern.replace("*", "(.*)") #variabler bereich
 pattern = pattern.replace(" ", "\s?") #leerzeichen variabel
 
-pattern = "("+ pattern +")"
+pattern = re.sub("(?:\*(\w*))", "(?P<\g<1>>.*)", pattern)
 
+pattern = "^("+ pattern +")$"
+
+
+
+
+#pattern = "((?:starte|beginne){0,1}\s?(\w*)\s?(?:wikipedia|wiki){1}\s?(?:suche){0,1}\s?(.*))"
 print(pattern)
- #https://regex101.com/r/gV0dP7/4
+print()
+
+p = re.compile(pattern)
+
+
+for checker in checkFor:
+
+    match = re.search(pattern, checker, re.MULTILINE|re.IGNORECASE)
+    if match:
+        print("sayed:  " + match.group(0))
+        print("found: " + match.group("suchStr"))
+        print()
+    else:
+        print("no match for: " + checker)
+        print()
